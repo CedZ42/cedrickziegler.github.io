16775adaf63fa98ae6332103d3f395c80ac13c7d
Cedrick <dcfzcedrick@gmail.com>
2024-05-05 19:14:58 +0000

    finished code comments

--- projects/runtime/js/levelData.js
+++ projects/runtime/js/levelData.js
@@ -15,19 +15,19 @@ var makeLevelData = function (window) {
         number: 1,
         speed: -3,
         gameItems: [
-          { type: "sawblade", x: 300, y: groundY - 120 },
-          { type: "sawblade", x: 700, y: groundY - 120 },
-          { type: "sawblade", x: 1000, y: groundY - 120 },
-          { type: "enemy", x: 800, y: groundY },
-          { type: "enemy", x: 1800, y: groundY },
-          { type: "enemy", x: 1200, y: groundY },
-          { type: "thug", x: 1400, y: groundY },
-          { type: "thug", x: 2400, y: groundY },
-          { type: "thug", x: 2300, y: groundY },
-          { type: "thug", x: 2500, y: groundY },
-          { type: "reward", x: 2450, y: groundY - 50 },
-          { type: "bestReward", x: 2500, y: groundY - 50 },
-          { type: "marker", x: 2400, y: groundY + 5 },
+          { type: "sawblade", x: 300, y: groundY - 120 }, // Creates a sawblade at X 300
+          { type: "sawblade", x: 700, y: groundY - 120 }, // Creates a sawblade at X 700
+          { type: "sawblade", x: 1000, y: groundY - 120 }, // Creates a sawblade at X 1000
+          { type: "enemy", x: 800, y: groundY }, // Creates an enemy at X 800
+          { type: "enemy", x: 1800, y: groundY }, // Creates an enemy at X 1800
+          { type: "enemy", x: 1200, y: groundY }, // Creates an enemy at X 1200
+          { type: "thug", x: 1400, y: groundY }, // Creates a thug at X 1400
+          { type: "thug", x: 2400, y: groundY }, // Creates a thug at X 2400
+          { type: "thug", x: 2300, y: groundY }, // Creates a thug at X 2300
+          { type: "thug", x: 2500, y: groundY }, // Creates a thug at X 2500
+          { type: "reward", x: 2450, y: groundY - 50 }, // Creates a reward at X 2450
+          { type: "bestReward", x: 2500, y: groundY - 50 }, // Creates a bestReward at X 2500
+          { type: "marker", x: 2400, y: groundY + 5 }, // Creates a marker at X 2400
         ],
       },
       {
@@ -35,17 +35,17 @@ var makeLevelData = function (window) {
         number: 2,
         speed: -3,
         gameItems: [
-          { type: "thug", x: 600, y: groundY },
-          { type: "thug", x: 700, y: groundY },
-          { type: "thug", x: 800, y: groundY },
-          { type: "thug", x: 900, y: groundY },
-          { type: "thug", x: 1000, y: groundY },
-          { type: "thug", x: 1100, y: groundY },
-          { type: "thug", x: 1200, y: groundY },
-          { type: "thug", x: 1300, y: groundY },
-          { type: "thug", x: 1400, y: groundY },
-          { type: "thug", x: 1500, y: groundY },
-          { type: "warrior", x: 2500, y: groundY },
+          { type: "thug", x: 600, y: groundY }, // Creates a thug at X 600
+          { type: "thug", x: 700, y: groundY }, // Creates a thug at X 700
+          { type: "thug", x: 800, y: groundY }, // Creates a thug at X 800
+          { type: "thug", x: 900, y: groundY }, // Creates a thug at X 900
+          { type: "thug", x: 1000, y: groundY }, // Creates a thug at X 1000
+          { type: "thug", x: 1100, y: groundY }, // Creates a thug at X 1100
+          { type: "thug", x: 1200, y: groundY }, // Creates a thug at X 1200
+          { type: "thug", x: 1300, y: groundY }, // Creates a thug at X 1300
+          { type: "thug", x: 1400, y: groundY }, // Creates a thug at X 1400
+          { type: "thug", x: 1500, y: groundY }, // Creates a thug at X 1500
+          { type: "warrior", x: 2500, y: groundY }, // Creates a warrior at X 2500
         ],
       },
     ];
--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -19,145 +19,144 @@ var runLevels = function (window) {
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
     function createSawBlade(x,y){
-      var hitZoneSize = 40;
-      var damageFromObstacle = 20;
-      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-      sawBladeHitZone.x = x; // Hitbox X value
-      sawBladeHitZone.y = y; // Hitbox Y value
+      var hitZoneSize = 40; // Variable for hitzone radius
+      var damageFromObstacle = 20; // Variable for obstacle radius
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);v // Creates the obstalce with the hitzone radius as hitZoneSize and damage with damageFromObstacle
+      sawBladeHitZone.x = x; // Assigns the x parameter in the function to the x value of the hitbox for the sawblade
+      sawBladeHitZone.y = y; // Assigns the y parameter in the function to the y value of the hitbox for the sawblade
       game.addGameItem(sawBladeHitZone); // Adds sawblades into game
-      var obstacleImage = draw.bitmap("img/bugs.png"); // Puts image on object
+      var obstacleImage = draw.bitmap("img/bugs.png"); // Inserts the image ontop of the obstacle
       obstacleImage.x = -40; // Picture X value
-      obstacleImage.y = -45; // Picture X value
-      obstacleImage.scaleX = .38; // X scale for picture
-      obstacleImage.scaleY = .38; // Y scale for picture
-      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.y = -45; // Picture Y value
+      obstacleImage.scaleX = .38; // Scales the X value for the picture
+      obstacleImage.scaleY = .38; // Scales the Y value for the picture
+      sawBladeHitZone.addChild(obstacleImage); // Groups the image with the sawblade
       
     }
 
     function createManhole(x, y) {
       var enemy = game.createGameItem("enemy", 25); // Creates game item
-      var manhole = draw.bitmap("img/manhole.png"); // Draws game item
-      manhole.scaleX = .5; // X scale for picture
-      manhole.scaleY = .75; // Y scale for picture
-      manhole.x = -125; // manhole X pos
-      manhole.y = -200; // manhole Y pos
-      enemy.addChild(manhole); // Relates the red square to the enemy
-      enemy.x = x; // Enemy X pos
-      enemy.y = y; // Enemy Y pos
+      var manhole = draw.bitmap("img/manhole.png"); // Inserts the image ontop of the manhole
+      manhole.scaleX = .5; // Scales the X value for the manhole picture
+      manhole.scaleY = .75; // Scales the Y value for the manhole picture
+      manhole.x = -125; // X position for manhole image
+      manhole.y = -200; // Y position for manhole image
+      enemy.addChild(manhole); // Groups the image with the enemy
+      enemy.x = x; // Assigns the x parameter in the function to the x value of the manhole position
+      enemy.y = y; // Assigns the y parameter in the function to the y value of the manhole position
       game.addGameItem(enemy); // Adds enemy into the game
-      enemy.velocityX = -5 // Enemy Movement
+      enemy.velocityX = -5 // Changes the X direction the manhole moves in
 
       enemy.onPlayerCollision = function () { // Function make the player lose health and object disappear
-        game.increaseScore(-100);
-        enemy.fadeOut();
+        game.increaseScore(-100); // Decreases the player's score on collision 
+        enemy.fadeOut(); // Makes enemy fade out on collision
       };
 
     }
 
     function createThug(x, y) {
       var enemy = game.createGameItem("enemy", 50); // Creates game item
-      var thug = draw.bitmap("img/thug.png"); // Draws game item
-      thug.scaleX = .5; // X scale for picture
-      thug.scaleY = .75; // Y scale for picture
-      thug.x = -80; // thug X pos
-      thug.y = -200; // thug Y pos
-      enemy.addChild(thug); // Relates the red square to the enemy
-      enemy.x = x; // Enemy X pos
-      enemy.y = y; // Enemy Y pos
-      game.addGameItem(enemy); // Adds enemy into the game
-      enemy.velocityX = -6 // Enemy Movement
+      var thug = draw.bitmap("img/thug.png"); // Inserts the image ontop of the thug
+      thug.scaleX = .5; // Scales the X value for the thug picture
+      thug.scaleY = .75; // Scales the Y value for the thug picture
+      thug.x = -80; // X position for thug image
+      thug.y = -200; // Y position for thug image
+      enemy.addChild(thug); // Groups the image with the thug
+      enemy.x = x; // Assigns the x parameter in the function to the x value of the thug position
+      enemy.y = y; // Assigns the y parameter in the function to the y value of the thug position
+      game.addGameItem(enemy); // Adds thug into the game
+      enemy.velocityX = -6 // Changes the X direction the thug moves in
 
       enemy.onPlayerCollision = function () { // Function make the player lose health and object disappear
-        game.increaseScore(-100);
-        enemy.fadeOut();
+        game.increaseScore(-100); // Decreases the player's score on collision 
+        enemy.fadeOut(); // Makes thug fade out on collision
       };
       enemy.onProjectileCollision = function () { // Function make the player get points and makes object disappaer
-        game.increaseScore(100);
-        enemy.fadeOut();
+        game.increaseScore(100); // Increases the player's score on bullet collision 
+        enemy.fadeOut(); // Makes thug fade out on bullet collision
       };
 
     }
 
     function createWarrior(x, y) {
       var enemy = game.createGameItem("enemy", 50); // Creates game item
-      var warrior = draw.bitmap("img/Warrior.webp"); // Draws game item
-      warrior.scaleX = .25; // X scale for picture
-      warrior.scaleY = .25; // Y scale for picture
-      warrior.x = -80; // warrior X pos
-      warrior.y = -200; // warrior Y pos
-      enemy.addChild(warrior); // Relates the red square to the enemy
-      enemy.x = x; // Enemy X pos
-      enemy.y = y; // Enemy Y pos
-      game.addGameItem(enemy); // Adds enemy into the game
-      enemy.velocityX = -6 // Enemy Movement
+      var warrior = draw.bitmap("img/Warrior.webp"); // Inserts the image ontop of the warrior
+      warrior.scaleX = .25; // Scales the X value for the warrior picture
+      warrior.scaleY = .25; // Scales the Y value for the warrior picture
+      warrior.x = -80; // X position for warrior image
+      warrior.y = -200; // Y position for warrior image
+      enemy.addChild(warrior); // Groups the image with the warrior
+      enemy.x = x; // Assigns the x parameter in the function to the x value of the warrior position
+      enemy.y = y; // Assigns the y parameter in the function to the y value of the warrior position
+      game.addGameItem(enemy); // Adds warrior into the game
+      enemy.velocityX = -6 // Changes the X direction the warrior moves in
 
       enemy.onPlayerCollision = function () { // Function make the player lose health and object disappear
-        game.increaseScore(-100);
-        enemy.fadeOut();
+        game.increaseScore(-100); // Decreases the player's score on collision
+        enemy.fadeOut(); // Makes warrior fade out on collision
       };
       enemy.onProjectileCollision = function () { // Function make the player get points and makes object disappaer
-        game.increaseScore(100);
-        enemy.fadeOut();
+        game.increaseScore(100); // Increases the player's score on bullet collision 
+        enemy.fadeOut(); // Makes warrior fade out on bullet collision
       };
     }
 
-    function createReward(x, y) {
+    function createReward(x) {
       var reward = game.createGameItem("enemy", 15); // Creates game item
-      var bandAid= draw.bitmap("img/Band-aid.png"); // Draws game item
-      bandAid.scaleX = .25; // X scale for picture
-      bandAid.scaleY = .25; // Y scale for picture
-      bandAid.x = -65; // blueSquare X pos
-      bandAid.y = -25; // blueSquare Y pos
-      reward.addChild(bandAid); // Relates the red square to the enemy
-      reward.x = x; // Enemy X pos
-      reward.y = groundY; // Enemy Y pos
-      game.addGameItem(reward); // Adds enemy into the game
-      reward.velocityX = -3 // Enemy Movement
+      var bandAid= draw.bitmap("img/Band-aid.png"); // Inserts the image ontop of the bandAid
+      bandAid.scaleX = .25; // Scales the X value for the bandAid picture
+      bandAid.scaleY = .25; // Scales the Y value for the bandAid picture
+      bandAid.x = -65; // X position for bandAid image
+      bandAid.y = -25; // Y position for bandAid image
+      reward.addChild(bandAid); // Groups the image with the bandAid
+      reward.x = x; // Assigns the x parameter in the function to the x value of the bandAid position
+      reward.y = groundY; // Assigns the groundY variable in the function to the y value of the bandAid position
+      game.addGameItem(reward); // Adds bandAid into the game
+      reward.velocityX = -3 // Changes the X direction the bandAid moves in
 
       reward.onPlayerCollision = function () { // Function make the player get points and makes object disappaer
-        game.increaseScore(100);
-        reward.fadeOut();
-        reward.shrink();
+        game.increaseScore(100); // Increases the player's score on collision 
+        reward.fadeOut(); // Makes bandAid fade out on collision
+        reward.shrink(); // Makes bandAid shrink on collision
       };
     }
 
     function createBetterReward(x) {
       var bestReward = game.createGameItem("enemy", 15); // Creates game item
-      var superBandAid= draw.bitmap("img/Superbandaid.png"); // Draws game item
-      superBandAid.scaleX = .15; // X scale for picture
-      superBandAid.scaleY = .15; // Y scale for picture
-      superBandAid.x = -40; // blueSquare X pos
-      superBandAid.y = -40; // blueSquare Y pos
-      bestReward.addChild(superBandAid); // Relates the red square to the enemy
-      bestReward.x = x; // Enemy X pos
-      bestReward.y = groundY; // Enemy Y pos
-      game.addGameItem(bestReward); // Adds enemy into the game
-      bestReward.velocityX = -3 // Enemy Movement
+      var superBandAid= draw.bitmap("img/Superbandaid.png"); // Inserts the image ontop of the Superbandaid
+      superBandAid.scaleX = .15; // Scales the X value for the Superbandaid picture
+      superBandAid.scaleY = .15; // Scales the Y value for the Superbandaid picture
+      superBandAid.x = -40; // X position for Superbandaid image
+      superBandAid.y = -40; // Y position for Superbandaid image
+      bestReward.addChild(superBandAid); // Groups the image with the Superbandaid
+      bestReward.x = x; // Assigns the x parameter in the function to the x value of the Superbandaid position
+      bestReward.y = groundY; // Assigns the groundY variable in the function to the y value of the Superbandaid position
+      game.addGameItem(bestReward); // Adds Superbandaid into the game
+      bestReward.velocityX = -3 // Changes the X direction the Superbandaid moves in
 
       bestReward.onPlayerCollision = function () { // Function make the player get points and makes object disappaer
-        game.increaseScore(100);
-        game.increaseScore(100);
-        bestReward.fadeOut();
-        bestReward.shrink();
+        game.increaseScore(200); // Increases the player's score on collision 
+        bestReward.fadeOut(); // Makes Superbandaid fade out on collision
+        bestReward.shrink(); // Makes Superbandaid shrink on collision
       };
     }
 
     function createMarker(x, y) {
       var marker = game.createGameItem("enemy", 25); // Creates game item
-      var yellowSquare= draw.rect(50, 50, "yellow"); // Draws game item
-      yellowSquare.x = -25; // marker X pos
-      yellowSquare.y = -25; // marker Y pos
-      marker.addChild(yellowSquare); // Relates the red square to the enemy
-      marker.x = x; // marker X pos
-      marker.y = y; // marker Y pos
-      game.addGameItem(marker); // Adds enemy into the game
-      marker.velocityX = -3; // Enemy Movement
-      yellowSquare.scaleX = 1; // Enemy Movement
-      yellowSquare.scaleY = 1; // Enemy Movement
+      var yellowSquare= draw.rect(50, 50, "yellow"); // Draws a yellow square ontop of the marker
+      yellowSquare.x = -25; // X position for yellow square image
+      yellowSquare.y = -25; // Y position for yellow square image
+      marker.addChild(yellowSquare); // Groups the yellow square with the marker
+      marker.x = x; // Assigns the x parameter in the function to the x value of the marker position
+      marker.y = y; // Assigns the y parameter in the function to the y value of the marker position
+      game.addGameItem(marker); // Adds marker into the game
+      marker.velocityX = -3; // Changes the X direction the Superbandaid moves in
+      yellowSquare.scaleX = 1; // Scales the X value for the yellow square
+      yellowSquare.scaleY = 1; // Scales the Y value for the yellow square
 
       marker.onPlayerCollision = function () { // Function makes marker disappear
-        marker.fadeOut();
-        startLevel();
+        marker.fadeOut(); // Makes marker fade out on collision
+        startLevel(); // Runs a function that increase the level variable to proceed from "Robot Romp" to the next level "Robot Rampage"
       };
 
     }
@@ -165,30 +164,30 @@ var runLevels = function (window) {
     function startLevel() {
       // TODO 13 goes below here
 
-      var level = levelData[currentLevel];
-      var levelObjects = level.gameItems;
-      for (var i = 0; i < levelObjects.length; i++){
+      var level = levelData[currentLevel]; // Variable that keeps track of what level you are on with the levelData array
+      var levelObjects = level.gameItems; // Variable that stores all game items in each level
+      for (var i = 0; i < levelObjects.length; i++){ // Loop that iterates through the levelObjects array to go through each game item in each level
         var element = levelObjects[i];
         if (element.type === "sawblade"){
-          createSawBlade(element.x, element.y);
+          createSawBlade(element.x, element.y); // If the type "sawblade" is up, run the createSawblade function with the element's x and y coorindates
         }
         if ( element.type === "enemy"){
-          createManhole(element.x,element.y);
+          createManhole(element.x,element.y); // If the type "enemy" is up, run the createManhole function with the element's x and y coorindates
         }
         if ( element.type === "reward"){
-          createReward(element.x,element.y);
+          createReward(element.x,element.y); // If the type "reward" is up, run the createReward function with the element's x and y coorindates
         }
         if (element.type === "marker"){
-          createMarker(element.x,element.y);
+          createMarker(element.x,element.y); // If the type "marker" is up, run the createMarker function with the element's x and y coorindates
         }
         if (element.type === "thug"){
-          createThug(element.x,element.y);
+          createThug(element.x,element.y); // If the type "thug" is up, run the createThug function with the element's x and y coorindates
         }
         if (element.type === "bestReward"){
-          createBetterReward(element.x,element.y);
+          createBetterReward(element.x,element.y); // If the type "bestReward" is up, run the createBetterReward function with the element's x and y coorindates
         }
         if (element.type === "warrior"){
-          createWarrior(element.x,element.y);
+          createWarrior(element.x,element.y); // If the type "warrior" is up, run the createWarrior function with the element's x and y coorindates
         }
       }
 
