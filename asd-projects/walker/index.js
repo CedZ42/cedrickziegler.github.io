95ec63b3dc9e152314a2093accbe26a23fa72f13
Cedrick <95383675+CedZ42@users.noreply.github.com>
2025-02-14 15:56:17 +0000

    fixed walker again

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -30,19 +30,6 @@ function runProgram(){
   var walker = Walker ("#walker", 0, 0, 0, 0, WALKER_WIDTH, WALKER_HEIGHT);
   var walker2 = Walker ("#walker2", BOARD_WIDTH - WALKER_WIDTH, BOARD_HEIGHT - WALKER_HEIGHT, 0, 0, WALKER_WIDTH, WALKER_HEIGHT);
 
-  function Walker (id, xPos, yPos, speedX, speedY, width, height){
-    let obj = {
-      id: id,
-      xPos: xPos,
-      yPos: yPos,
-      speedX: speedX,
-      speedY: speedY,
-      width: width,
-      height: height,
-    }
-    return obj;
-  }
-
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
@@ -57,9 +44,12 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    repositionGameItem();
-    wallCollision();
-    redrawGameItem();
+    repositionGameItem(walker);
+    repositionGameItem(walker2);
+    wallCollision(walker);
+    wallCollision(walker2);
+    redrawGameItem(walker);
+    redrawGameItem(walker2);
     handleCollision();
   }
   
@@ -114,6 +104,19 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function Walker (id, xPos, yPos, speedX, speedY, width, height){
+    let obj = {
+      id: id,
+      xPos: xPos,
+      yPos: yPos,
+      speedX: speedX,
+      speedY: speedY,
+      width: width,
+      height: height,
+    }
+    return obj;
+  }
+
   function handleCollision(){
     if (doCollide(walker, walker2)){
       walker.xPos = 0;
@@ -153,33 +156,23 @@ function runProgram(){
 		
 }
   
-  function wallCollision(){
-    if (walker.xPos > BOARD_WIDTH - WALKER_WIDTH || walker.xPos < 0){
-      walker.xPos -= walker.speedX;
+  function wallCollision(obj){
+    if (obj.xPos > BOARD_WIDTH - WALKER_WIDTH || obj.xPos < 0){
+      obj.xPos -= obj.speedX;
     }
-    if (walker.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker.yPos < 0){
-      walker.yPos -= walker.speedY;
-    }
-    if (walker2.xPos > BOARD_WIDTH - WALKER_WIDTH || walker2.xPos < 0){
-      walker2.xPos -= walker2.speedX;
-    }
-    if (walker2.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker2.yPos < 0){
-      walker2.yPos -= walker2.speedY;
+    if (obj.yPos > BOARD_HEIGHT - WALKER_HEIGHT || obj.yPos < 0){
+      obj.yPos -= obj.speedY;
     }
   }
 
-  function repositionGameItem(){
-    walker.xPos += walker.speedX;
-    walker.yPos += walker.speedY;
-    walker2.xPos += walker2.speedX;
-    walker2.yPos += walker2.speedY;
+  function repositionGameItem(obj){
+    obj.xPos += obj.speedX;
+    obj.yPos += obj.speedY;
   }
 
-  function redrawGameItem(){
-    $("#walker").css("left", walker.xPos);
-    $("#walker").css("top", walker.yPos);
-    $("#walker2").css("left", walker2.xPos);
-    $("#walker2").css("top", walker2.yPos);
+  function redrawGameItem(obj){
+    $(obj.id).css("left", obj.xPos);
+    $(obj.id).css("top", obj.yPos);
   }
 
   function changeColor (){
